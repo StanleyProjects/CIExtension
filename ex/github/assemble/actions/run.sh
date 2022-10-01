93b22068ac2de032079a5c7dae4b68048b926ccc
Stanley Wintergreen <kepocnhh@gmail.com>
2022-10-01 20:22:54 +0300

    check github assemblle actions run

--- ex/github/assemble/actions/run.sh
+++ ex/github/assemble/actions/run.sh
@@ -8,12 +8,12 @@ mkdir -p assemble/vcs/actions \
  || . ex/util/throw 11 "Illegal state!"
 
 CODE=0
-CODE=$(curl -s -w %{http_code} -o assemble/vcs/actions/run.json \
+CODE=$(curl -s -w %{http_code} -m 16 -o assemble/vcs/actions/run.json \
  "$VCS_DOMAIN/repos/$REPOSITORY_OWNER/$REPOSITORY_NAME/actions/runs/$CI_BUILD_ID")
 if test $CODE -ne 200; then
- echo "Get actions run $CI_BUILD_ID error!"
+ echo "Get actions run \"$CI_BUILD_ID\" error!"
  echo "Request error with response code $CODE!"
- exit 11
+ exit 21
 fi
 
 . ex/util/json -f assemble/vcs/actions/run.json \
