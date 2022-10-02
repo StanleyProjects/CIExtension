32f0f3e619cadca6bb5169b0c4f625283bb7602d
Stanley Wintergreen <kepocnhh@gmail.com>
2022-10-02 15:30:23 +0300

    github assemble repository

---
+++ ex/github/assemble/repository.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Assemble GitHub repository..."
+
+. ex/util/json -f assemble/vcs/actions/run.json \
+ -sfs .repository.url REPOSITORY_URL \
+ -sfs .repository.name REPOSITORY_NAME
+
+ex/util/url "$REPOSITORY_URL" \
+ assemble/vcs/repository.json \
+ || . ex/util/throw 21 "Get repository \"$REPOSITORY_NAME\" error!"
+
+. ex/util/json -f assemble/vcs/repository.json \
+ -sfs .html_url REPOSITORY_HTML_URL
+
+echo "The repository $REPOSITORY_HTML_URL is ready."
