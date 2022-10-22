984f820ef1c16ffec18d3a750be8ab0b6a4eb65c
Stanley Wintergreen <kepocnhh@gmail.com>
2022-10-22 20:54:15 +0300

    check util pipeline

--- internal/check/util/pipeline/build.sh
+++ internal/check/util/pipeline/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-DOCKERFILE="internal/check/util/pipeline/Dockerfile"
-NAME="$(md5sum <<< "$DOCKERFILE" | base64)"
+RELATIVE_PATH='internal/check/util/pipeline'
+DOCKERFILE="$RELATIVE_PATH/Dockerfile"
+NAME="$(md5sum <<< "$RELATIVE_PATH" | base64)"
 NAME="${NAME,,}"
-VERSION=$(date +%s)
+VERSION=$(date +"%Y%m%d%H")
 TAG="$NAME:$VERSION"
 CONTAINER="container.$NAME"
 
@@ -12,17 +13,13 @@ docker rm "$CONTAINER"
 
 CODE=0
 docker build --no-cache -f="$DOCKERFILE" -t="$TAG" . \
- && docker run -td --name="$CONTAINER" "$TAG" \
- && docker exec "$CONTAINER" check/success.sh \
- && docker exec "$CONTAINER" check/error.sh; CODE=$?
+ && docker run --rm --name="$CONTAINER" "$TAG"; CODE=$?
 
 if test $CODE -ne 0; then
  echo "Build error!"
  exit 21
 fi
 
-docker stop "$CONTAINER"
-docker rm "$CONTAINER"
 docker rmi "$TAG"
 
 exit 0
