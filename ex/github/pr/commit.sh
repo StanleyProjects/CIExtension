b4bd6e5b35b55c293b571b17bf9b97173339c03f
Stanley Wintergreen <kepocnhh@gmail.com>
2022-10-24 23:59:13 +0300

    github pr commit

---
+++ ex/github/pr/commit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "GitHub pull request commit..."
+
+. ex/util/require PR_NUMBER
+
+. ex/util/json -f "assemble/vcs/pr${PR_NUMBER}.json" \
+ -sfs .head.sha GIT_COMMIT_SRC \
+ -sfs .base.sha GIT_COMMIT_DST
+
+. ex/util/json -f assemble/vcs/actions/run.json \
+ -si .run_number CI_BUILD_NUMBER
+
+REPOSITORY=repository
+. ex/util/assert -d "$REPOSITORY"
+
+MESSAGE="Merge ${GIT_COMMIT_SRC::7} -> ${GIT_COMMIT_DST::7} by CI build #${CI_BUILD_NUMBER}."
+git -C "$REPOSITORY" commit -m "$MESSAGE" \
+ || . ex/util/throw 21 "Git commit error!"
