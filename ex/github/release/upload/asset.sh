11295e6d122bdf01a110667e6a2d3cb721ea8d53
Stanley Wintergreen <kepocnhh@gmail.com>
2022-12-09 16:48:24 +0300

    github release upload asset

---
+++ ex/github/release/upload/asset.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "GitHub release upload asset..."
+
+if test $# -ne 1; then
+ echo "Script needs for 1 argument, but actual is $#!"; exit 11; fi
+
+ASSETS="$1"
+
+. ex/util/require ASSETS VCS_PAT
+
+exit 1 # todo json select filled array
