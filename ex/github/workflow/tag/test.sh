f952c2e9359d6a5a69421f95fcb92b98f2c82e9f
Stanley Wintergreen <kepocnhh@gmail.com>
2022-12-13 20:07:01 +0300

    github workflow tag test | $(date +%s)

--- ex/github/workflow/tag/test.sh
+++ ex/github/workflow/tag/test.sh
@@ -2,8 +2,11 @@
 
 echo "GitHub workflow tag test..."
 
-. ex/util/require TAG
+if test $# -ne 1; then
+ echo "Script needs for 1 argument, but actual is $#!"; exit 11; fi
+
+TAG="$1"
 
 ex/github/tag/test.sh "$TAG"
 if test $? -ne 0; then
- ex/github/workflow/tag/test/on_failed.sh; exit 11; fi
+ ex/github/workflow/tag/test/on_failed.sh; exit 21; fi
