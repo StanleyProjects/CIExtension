ca2ec8392b21099274cc5a53c9d3dad26d5dc553
Stanley Wintergreen <kepocnhh@gmail.com>
2022-12-18 18:56:29 +0300

    workflow pull request unstable | 1671378989

--- internal/shell/workflow/pull/request/setup.sh
+++ internal/shell/workflow/pull/request/setup.sh
@@ -3,9 +3,9 @@
 echo 'Workflow pull request setup'
 
 . ex/util/pipeline \
- ex/github/assemble/repository.sh \
+ ex/github/assemble/actions/run.sh \
+ ex/github/assemble/actions/run/repository.sh \
  ex/github/assemble/repository/owner.sh \
  ex/github/assemble/worker.sh \
  ex/github/assemble/pr.sh \
- ex/github/assemble/pr/commit.sh \
- ex/github/assemble/actions/run.sh
+ ex/github/assemble/pr/commit.sh
