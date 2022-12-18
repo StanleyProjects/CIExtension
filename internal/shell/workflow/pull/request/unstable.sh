40caad927a975683e19faf900620bee34b4e503c
Stanley Wintergreen <kepocnhh@gmail.com>
2022-12-18 22:24:16 +0300

    internal | workflow pull request unstable

--- internal/shell/workflow/pull/request/unstable.sh
+++ internal/shell/workflow/pull/request/unstable.sh
@@ -2,4 +2,10 @@
 
 echo 'Workflow pull request unstable'
 
-echo "Not implemented!"; exit 3 # todo
+. ex/util/assert -s internal/env
+. internal/env
+. ex/util/require VERSION
+
+ex/github/workflow/tag/test.sh "${VERSION}-UNSTABLE"
+
+echo 'Not implemented!'; exit 3 # todo
