7a021365baf492993333b84dac8736a5d7e438c2
Stanley Wintergreen <kepocnhh@gmail.com>
2022-12-22 17:56:34 +0300

    check github workflow tag test on failed

---
+++ internal/check/github/workflow/tag/test/on_failed/check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT='ex/github/workflow/tag/test/on_failed.sh'
+. ex/util/assert -s "$SCRIPT"
+
+echo '
+Check error...'
+
+echo 'Not implemented!'; exit 1 # todo
+
+echo '
+Check success...'
+
+echo 'Not implemented!'; exit 1 # todo
+
+exit 0
